01665007d4980284413cecd2a6b6083e6c98b309
kacpkedz <144997737+kacpkedz@users.noreply.github.com>
2024-02-03 01:30:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const przycisk_tak = document.getElementById('tak');
+
+przycisk_tak.addEventListener('click', function() {
+  window.location.href = 'tak.html';
+});
+
+const przycisk_nie = document.getElementById('nie');
+
+przycisk_nie.addEventListener('click', function() {
+  window.location.href = 'nie.html';
+});
